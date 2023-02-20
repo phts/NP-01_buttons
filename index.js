2d9b18ef656b822ae330629812710487fb851446
Phil Tsaryk <phil@tsaryk.com>
2023-02-20 16:31:22 +0100

    feat: add Shutdown button

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const REPEAT_DELAY = 750
 const APP_DIR = '/home/volumio/NP-01_buttons'
 const BUTTONS = [
   {pin: 27, clickCmd: 'volumio toggle'},
+  {
+    pin: 26,
+    clickCmd: 'volumio clear && sleep 1 && systemctl poweroff',
+    holdCmd: 'volumio clear && sleep 1 && systemctl reboot',
+    holdOnce: true,
+  },
   {pin: 24, clickCmd: `bash ${APP_DIR}/exit_vu_meter.sh`},
   {pin: 22, clickCmd: 'volumio previous', holdCmd: 'volumio seek minus'},
   {pin: 17, clickCmd: 'volumio next', holdCmd: 'volumio seek plus'},
@@ -24,7 +30,7 @@ function exec(cmd) {
   }
 }
 
-const buttons = BUTTONS.map(({pin, clickCmd, holdCmd}) => {
+const buttons = BUTTONS.map(({pin, clickCmd, holdCmd, holdOnce}) => {
   const btn = new Gpio(pin, 'in', holdCmd ? 'both' : 'rising', {debounceTimeout: 100})
   btn.watch((err, pressed) => {
     if (err) {
@@ -37,6 +43,9 @@ const buttons = BUTTONS.map(({pin, clickCmd, holdCmd}) => {
     if (pressed) {
       holdInterval = setInterval(() => {
         holded = true
+        if (holdOnce) {
+          clearInterval(holdInterval)
+        }
         exec(holdCmd)
       }, REPEAT_DELAY)
       return
