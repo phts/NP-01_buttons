b467a6e3a930c5f6d359f9b19ebc1f5ad8da7348
Phil Tsaryk <phil@tsaryk.com>
2023-02-20 13:48:48 +0100

    feat: add previous and next buttons

--- index.js
+++ index.js
@@ -3,22 +3,57 @@
 const {execSync} = require('child_process')
 const Gpio = require('onoff').Gpio
 
+const REPEAT_DELAY = 750
 const APP_DIR = '/home/volumio/NP-01_buttons'
-const GPIO_PIN = 24
-const button = new Gpio(GPIO_PIN, 'in', 'rising', {debounceTimeout: 100})
+const HOME_BTN_GPIO_PIN = 24
+const PREV_BTN_GPIO_PIN = 22
+const NEXT_BTN_GPIO_PIN = 17
+const homeButton = new Gpio(HOME_BTN_GPIO_PIN, 'in', 'rising', {debounceTimeout: 100})
+const prevButton = new Gpio(PREV_BTN_GPIO_PIN, 'in', 'both', {debounceTimeout: 100})
+const nextButton = new Gpio(NEXT_BTN_GPIO_PIN, 'in', 'both', {debounceTimeout: 100})
+let holdInterval
+let holded = false
 
-button.watch((err) => {
-  if (err) {
-    throw err
-  }
+function exec(cmd) {
   try {
-    execSync(`bash ${APP_DIR}/exit_vu_meter.sh >> ${APP_DIR}/buttons.log 2>&1`)
+    execSync(`${cmd} >> ${APP_DIR}/buttons.log 2>&1`)
   } catch (e) {
     console.error(e.stdout.toString())
     console.error(e.stderr.toString())
   }
+}
+
+homeButton.watch((err) => {
+  if (err) {
+    throw err
+  }
+  exec(`bash ${APP_DIR}/exit_vu_meter.sh`)
+})
+;[
+  [prevButton, 'volumio seek minus', 'volumio previous'],
+  [nextButton, 'volumio seek plus', 'volumio next'],
+].forEach(([btn, holdCmd, clickCmd]) => {
+  btn.watch((err, pressed) => {
+    if (err) {
+      throw err
+    }
+    if (pressed) {
+      holdInterval = setInterval(() => {
+        holded = true
+        exec(holdCmd)
+      }, REPEAT_DELAY)
+      return
+    }
+    clearInterval(holdInterval)
+    if (!holded) {
+      exec(clickCmd)
+    }
+    holded = false
+  })
 })
 
 process.on('SIGINT', () => {
-  button.unexport()
+  homeButton.unexport()
+  prevButton.unexport()
+  nextButton.unexport()
 })
