ac21c10966680919b3d1cf219b0518e89b1f0cb3
Phil Tsaryk <phil@tsaryk.com>
2023-02-20 14:01:35 +0100

    feat: move logging control to run.sh instead of rc.local

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-cd /home/volumio/NP-01_buttons
 {
-  npm i --omit=dev
-} || {
-  sleep 10 && npm i --omit=dev
-}
-node index.js
+  cd /home/volumio/NP-01_buttons
+  {
+    npm i --omit=dev
+  } || {
+    sleep 10 && npm i --omit=dev
+  }
+  node index.js
+} >/home/volumio/NP-01_buttons/run.log 2>&1
