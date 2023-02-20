5e8e01a4ddcb48da75bf9198e32e9ec536722747
Phil Tsaryk <phil@tsaryk.com>
2023-02-20 14:12:03 +0100

    fix: support npm v6

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ set -ex
 {
   cd /home/volumio/NP-01_buttons
   {
-    npm i --omit=dev
+    npm i --only=prod
   } || {
-    sleep 10 && npm i --omit=dev
+    sleep 10 && npm i --only=prod
   }
   node index.js
 } >/home/volumio/NP-01_buttons/run.log 2>&1
