b775b6a2dd06d5fb2bcde86ea605ebc8adf83b18
Phil Tsaryk <phil@tsaryk.com>
2023-02-20 13:40:07 +0100

    style: reformat

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const { execSync } = require("child_process");
-const Gpio = require('onoff').Gpio;
+'use strict'
 
-const APP_DIR = '/home/volumio/NP-01_home-button';
-const GPIO_PIN = 24;
-const button = new Gpio(GPIO_PIN, 'in', 'rising', {debounceTimeout: 100});
+const {execSync} = require('child_process')
+const Gpio = require('onoff').Gpio
 
-button.watch((err, value) => {
+const APP_DIR = '/home/volumio/NP-01_home-button'
+const GPIO_PIN = 24
+const button = new Gpio(GPIO_PIN, 'in', 'rising', {debounceTimeout: 100})
+
+button.watch((err) => {
   if (err) {
-    throw err;
+    throw err
   }
   try {
-    execSync(`bash ${APP_DIR}/exit_vu_meter.sh >> ${APP_DIR}/home-button.log 2>&1`);
-  } catch(e) {
+    execSync(`bash ${APP_DIR}/exit_vu_meter.sh >> ${APP_DIR}/home-button.log 2>&1`)
+  } catch (e) {
     console.error(e.stdout.toString())
     console.error(e.stderr.toString())
   }
-});
+})
 
-process.on('SIGINT', _ => {
-  button.unexport();
-});
+process.on('SIGINT', () => {
+  button.unexport()
+})
