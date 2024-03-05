68bd97c0c8d522fc3cf47ff3901ca88cc2fa8d28
Phil Tsaryk <phil@tsaryk.com>
2024-03-05 18:25:07 +0100

    feat: use "toggleStopAfterCurrent" websocket command

--- app/commands/toggle-stop-after-current.js
+++ app/commands/toggle-stop-after-current.js
@@ -1,32 +1,5 @@
 'use strict'
-const {execSync} = require('child_process')
 
-const FILENAME = '/stop-after-current.js'
+const socket = require('socket.io-client').connect('http://localhost:3000')
 
-const ids = execSync(`ps ax | grep "${FILENAME}"  | grep -v grep | awk '{print $1}'`)
-  .toString()
-  .split('\n')
-  .filter((x) => x)
-
-if (ids.length) {
-  // already on
-  ids.forEach((id) => {
-    try {
-      execSync(`kill ${id}`)
-    } catch (e) {
-      // ignore
-    }
-  })
-  const socket = require('socket.io-client').connect('http://localhost:3000')
-  socket.emit('pushToastMessage', {type: 'info', title: 'Stop after current', message: 'Off'})
-  process.exit()
-} else {
-  // off
-  try {
-    const output = execSync(`node ${__dirname}/${FILENAME}`)
-    console.debug(output.toString())
-  } catch (e) {
-    console.error(e.stderr ? e.stderr.toString() : e)
-    console.error(e.stdout ? e.stdout.toString() : e)
-  }
-}
+socket.emit('toggleStopAfterCurrent')
